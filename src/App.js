07a0e38d06ba816b309dc6137ed44f9f50e86303
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2022-05-22 02:45:49 +0200

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          this chenge to prove CI/CD.
         </p>
         <a
           className="App-link"
